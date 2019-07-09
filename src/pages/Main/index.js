ef656c05898c7e42bb2e9c691ae3de2b3883bb42
Pedro Lucas <49085137+Plucks77@users.noreply.github.com>
2019-07-09 00:49:19 -0300

    Nenhuma alteração

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from "react";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-import {Animated} from 'react-native';
-import {PanGestureHandler, State} from 'react-native-gesture-handler'
+import { Animated } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
 
-import Header from '~/components/Header';
-import Tabs from '~/components/Tabs';
-import Menu from '~/components/Menu';
+import Header from "~/components/Header";
+import Tabs from "~/components/Tabs";
+import Menu from "~/components/Menu";
 
-import {Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation
+} from "./styles";
 
-
-export default function Main(){
+export default function Main() {
   let offset = 0;
   const translateY = new Animated.Value(0);
 
@@ -19,37 +28,37 @@ export default function Main(){
     [
       {
         nativeEvent: {
-          translationY: translateY,
+          translationY: translateY
         }
       }
     ],
-    { useNativeDriver: true},
+    { useNativeDriver: true }
   );
 
-  function onHandlerStateChanged(event){
-    if(event.nativeEvent.oldState == State.ACTIVE){
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState == State.ACTIVE) {
       let opened = false;
-      const {translationY} = event.nativeEvent;
+      const { translationY } = event.nativeEvent;
 
       offset += translationY;
 
-      if (translationY >= 100){
-        opened= true;  
-      }else{
+      if (translationY >= 100) {
+        opened = true;
+      } else {
         translateY.setValue(offset);
         translateY.setOffset(0);
-        offset= 0;
+        offset = 0;
       }
 
       Animated.timing(translateY, {
         toValue: opened ? 380 : 0,
         duration: 200,
-        useNativeDriver: true,
-      }).start(()=>{
+        useNativeDriver: true
+      }).start(() => {
         offset = opened ? 380 : 0;
         translateY.setOffset(offset);
         translateY.setValue(0);
-      });      
+      });
     }
   }
   return (
@@ -57,41 +66,41 @@ export default function Main(){
       <Header />
       <Content>
         <Menu translateY={translateY} />
-        <PanGestureHandler 
-        onGestureEvent={animatedEvent}
-        onHandlerStateChanged={onHandlerStateChanged}
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChanged={onHandlerStateChanged}
         >
-        <Card style={{
-          transform: [{
-            translateY: translateY.interpolate({
-              inputRange:[-350, 0, 380],
-              outputRange: [-50, 0, 380],
-              extrapolate: 'clamp',
-            }),
-          }],
-        }}>          
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description> R$197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebido de Pedro Lucas hoje às 06:00
-            </Annotation>
-          </CardFooter>
-        </Card>
-        </PanGestureHandler> 
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: "clamp"
+                  })
+                }
+              ]
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description> R$197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebido de Pedro Lucas hoje às 06:00
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs translateY={translateY}/>
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
-
-
-
-
